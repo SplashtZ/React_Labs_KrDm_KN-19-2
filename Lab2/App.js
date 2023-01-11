29148411afb11aa56b8d06a0899f1661b5f87962
Dmytro Kravchuk <kn192_kda@student.ztu.edu.ua>
2023-01-11 20:05:22 +0200

    ReactLabs

---
+++ Lab2/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CounterList from "./components/task1/CounterList";
+import Cart from "./components/task2/Cart";
+import Game from "./components/task3/Game";
+
+const counters = [
+    { id: 1, initial: 6, min: -5, max: 10 },
+    { id: 2, initial: 5 },
+    { id: 3 },
+];
+
+const cartList = [
+    { id: 1, name: "Constructor LEGO", price: 300, min: 0, max: 5 },
+    { id: 2, name: "Train Station", price: 200, min: 0, max: 4 },
+    { id: 3, name: "Hot Wheels Track", price: 150, min: 0, max: 6 },
+];
+
+function App() {
+    return (
+        <div className="App">
+            <CounterList countersList={counters} />
+            <Cart cartList={cartList} />
+            <Game />
+        </div>
+    );
+}
+
+export default App;
---
+++ Lab7/toolkitRedux/createSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../store/actions/users";
+
+const toolkitSlice = createSlice({
+    name: "toolkit",
+    initialState: {
+        counter: 20,
+        users: [],
+        status: null,
+        error: null,
+    },
+    reducers: {
+        increment(state) {
+            state.counter = state.counter + 10;
+        },
+        decrement(state) {
+            state.counter = state.counter - 10;
+        },
+        addUser(state, action) {
+            state.users.push(action.payload);
+        },
+        removeUser(state, action) {
+            state.users = state.users.filter((user) => user.id !== action.payload);
+        },
+    },
+    extraReducers: {
+        [fetchUsers.pending]: (state) => {
+            state.status = "loading";
+            state.error = null;
+        },
+        [fetchUsers.fulfilled]: (state, action) => {
+            state.status = "resolved";
+            state.users.push(...action.payload);
+        },
+    },
+});
+
+export default toolkitSlice.reducer;
+export const { increment, decrement, addUser, removeUser } =
+    toolkitSlice.actions;
